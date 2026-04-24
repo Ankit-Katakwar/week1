afbb83dfa2c5e72ba503ca51b2f0118311334e53
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-04-24 08:42:52 +0530

    entries done

--- day1.js
+++ day1.js
@@ -1,20 +1,11 @@
-const company = {
-  name: "Katakwar Coal",
-  owner: {
-    name: "Ankit",
-    age: 23,
-    contact: {
-      phone: "9876543210",
-      email: "ankit@coal.com"
-    }
-  },
-  inventory: ["Bituminous", "Anthracite"],
-  getOwnerEmail() {
-    return this.owner.contact.email
-  }
-}
 
-// Access karna
-console.log(company.owner.name)              // Ankit
-console.log(company.owner.contact.phone)     // 9876543210
-console.log(company.getOwnerEmail())         // ankit@coal.com
+
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+const user = { name: "Ankit", age: 23, city: "Bhopal" }
+
+Object.entries(user).forEach(([key,value])=>{
+  console.log(`${key} : ${value}`)
+})
--- day3.js
+++ day3.js
@@ -1,19 +1,38 @@
-const company = {
-  name: "Katakwar Coal",
-  owner: {
-    name: "Ankit",
-    city: "Bhopal",
-  },
-};
+const user = { name: "Ankit", age: 23 } 
 
-// Destructuring
-// const {name:businessName,owner:{name:ownerName , city}} = company
+const userCopy = {...user}
+const extras = {city:"Bhopal",pet:true}
 
-// console.log(ownerName)
-// console.log(businessName)
+const fullUser = {...user , ...extras}
 
-function greet({ name:businessName,owner: { name: ownerNaam, city } }) {
-  console.log(`My name is ${ownerNaam} and I live in ${city} also I run a business called ${businessName}`);
-}
+const update = {...user, name:"Ishit"}
+user.name = "aman"
 
-greet(company);
+const user2 ={name:"vidit",age:32,location:{
+  state:"M.P",
+  city:"Pipariya",
+  pincode:"461775"
+}}
+
+// const copyUser2 = {...user2} 
+
+// copyUser2.name = "haggu"
+// copyUser2.location.city = "Bhopal"
+
+
+const deepCopyUser2 = JSON.parse(JSON.stringify(user2))
+
+deepCopyUser2.location.city = "delhi"
+
+
+// console.log(fullUser)
+// console.log(update)
+// console.log(user)
+// console.log(user2)
+// console.log(deepCopyUser2)
+
+
+
+
+
+
