51ea4879874fb0007e1593200a5b1d950e0a6afb
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-04-25 23:07:57 +0530

    Revised

--- day1.js
+++ day1.js
@@ -3,6 +3,7 @@
 // console.log(Object.keys(user))
 // console.log(Object.values(user))
 // console.log(Object.entries(user))
+// live form 
 
 const user = { name: "Ankit", age: 23, city: "Bhopal" }
 
