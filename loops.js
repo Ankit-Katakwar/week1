46db9321b884e0d33fa1f147948c249a7e625340
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-04-26 07:14:36 +0530

    advance practice

---
+++ loops.js
@@ -0,0 +1,16 @@
+// LOOPS PRACTICE - DAY 1
+
+console.log("Start Practice");
+
+// Write your code below 👇
+
+
+let totalCount = 0 ;
+
+for (let i = 1 ; i<=100 ;i++){
+
+    if(i%15==0){
+        totalCount++
+    }
+}
+console.log(totalCount)
