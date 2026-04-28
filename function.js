a804fde831830aa29bd1fa325efb0183e827096f
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-04-28 23:45:29 +0530

    complete advanced loops algo2

---
+++ function.js
@@ -0,0 +1,21 @@
+console.log("Hello function");
+
+function getEven(nums) {
+   let max = null;
+   let secondMax = 0;
+  for (let num of nums) {
+    if (num % 2 === 0) {
+
+      if (num > max) {
+
+        max = num;
+        
+      }
+      if(max > secondMax) {
+         secondMax = secondMax
+      }
+   }
+}
+console.log(secondMax)  
+}
+console.log(getEven([3,5,4,6,8]))
